2de93e61f511444b9c3a40877e0309a77559d7e7
pacificLiving <kyle.corbelli@gmail.com>
2016-01-13 14:42:14 -0800

    Create stackSize variable and allow it to be altered and displayed. Also, return 0 if stackSize is negative.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,23 @@
 var Stack = function() {
   var someInstance = {};
 
+  var stackSize = 0;
+
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    stackSize++;
   };
 
   someInstance.pop = function() {
+    stackSize--;
   };
 
   someInstance.size = function() {
+    // return Math.max(stackSize, 0);
+    return (stackSize >= 0) ? stackSize : 0;
   };
 
   return someInstance;
