08843536989aec372ec2cda2dad0c817fa9651a1
pacificLiving <kyle.corbelli@gmail.com>
2016-01-13 15:38:32 -0800

    Create structure for queue methods, create size method, and assign methods to the someInstance object.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  
+  var someInstance = {};
+  someInstance.counter = 0;
+  someInstance.storage = {}
+
+  someInstance.pop = queueMethods.pop;
+  someInstance.push = queueMethods.push;
+  someInstance.size = queueMethods.size;
+
+  return someInstance;
+
 };
 
-var queueMethods = {};
+var queueMethods = {
+  pop: function() {
 
+  },
+  push: function() {
 
+  },
+  size: function() {
+
+    return this.counter;
+  }
+};
+
+var test = Queue();
+console.log(test.size());
