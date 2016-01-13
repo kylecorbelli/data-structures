95ae299851e51f63c6dac59916aba331be1f3097
pacificLiving <kyle.corbelli@gmail.com>
2016-01-13 15:14:41 -0800

    Create seperate varibale to keep track of stackSize, impliment enqueue and dequeue.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,32 @@
 var Queue = function() {
   var someInstance = {};
 
+  var stackSize = 0;
+
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    stackSize++;
+    for (var i = stackSize; i > 0; i--) {
+      storage[i] = storage[i - 1]
+    }
+    storage[0] = value;
   };
 
   someInstance.dequeue = function() {
+    if (stackSize > 0) {
+      stackSize--;
+      var cache = storage[stackSize];
+      delete storage[stackSize];
+      return cache;
+    }
   };
 
   someInstance.size = function() {
+    return stackSize;
   };
 
   return someInstance;
