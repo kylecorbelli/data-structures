176b74eb0953c663b3ddcf6b52e108f5916b0857
pacificLiving <kyle.corbelli@gmail.com>
2016-01-14 11:05:10 -0800

    Pass all tests for sprint-two linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,41 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var recurse = function(passedNode) {
+      if (passedNode.next === null) {
+        passedNode.next = Node(value);
+        list.tail = passedNode.next;
+      } else {
+        recurse(passedNode.next);
+      }
+    };
+    if (list.head === null) {
+      list.head = Node(value);
+      list.tail = Node(value);
+    }
+    else {
+      recurse(list.head);
+    }
   };
 
   list.removeHead = function() {
+    if (list.head !== null) {
+      var cache = list.head.value;
+      list.head = list.head.next;
+      return cache;      
+    }
   };
 
   list.contains = function(target) {
+    return (function recurse(passedNode) {
+      if (passedNode.value === target) {
+        return true;
+      }
+      if (passedNode.next !== null) {
+        return recurse(passedNode.next);
+      }
+      return false;
+    })(list.head);
   };
 
   return list;
@@ -27,3 +56,13 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+var example = {
+  value: 'x',
+  next: {
+    value: 'y',
+    next: {
+      value: 'z',
+      next: null
+    }
+  }
+};
