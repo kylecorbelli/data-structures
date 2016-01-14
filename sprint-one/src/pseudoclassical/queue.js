d2e10909f703b4aacfbbdae782672e0d205ec806
pacificLiving <kyle.corbelli@gmail.com>
2016-01-13 19:16:22 -0800

    Define pseudoclassical constructor and prototype

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
 };
 
-
+Queue.prototype = {
+  enqueue: function(value){
+    for (var i = this.count; i > 0; i--) {
+      this.storage[i] = this.storage[i - 1];
+    }
+    this.storage[0] = value;
+    this.count++;
+  },
+  dequeue: function(){
+    if (this.count > 0) {
+      var cache = this.storage[--this.count];
+      delete this.storage[this.count];
+      return cache;
+    }
+  },
+  size: function(){
+    return this.count;
+  }
+};
