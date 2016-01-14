1ad335724c6cd2c7658a65117ab6a15f96fbde22
Kyle Corbelli <kyle.corbelli@RF.LAN>
2016-01-13 19:53:38 -0700

    Refactor pseudoclassical prototype model to simply add methods instead of overwriting the 'Queue.prorotype' object

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -3,22 +3,22 @@ var Queue = function() {
   this.storage = {};
 };
 
-Queue.prototype = {
-  enqueue: function(value){
-    for (var i = this.count; i > 0; i--) {
-      this.storage[i] = this.storage[i - 1];
-    }
-    this.storage[0] = value;
-    this.count++;
-  },
-  dequeue: function(){
-    if (this.count > 0) {
-      var cache = this.storage[--this.count];
-      delete this.storage[this.count];
-      return cache;
-    }
-  },
-  size: function(){
-    return this.count;
+Queue.prototype.enqueue = function(value) {
+  for (var i = this.count; i > 0; i--) {
+    this.storage[i] = this.storage[i - 1];
   }
+  this.storage[0] = value;
+  this.count++;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.count > 0) {
+    var cache = this.storage[--this.count];
+    delete this.storage[this.count];
+    return cache;
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.count;
 };
