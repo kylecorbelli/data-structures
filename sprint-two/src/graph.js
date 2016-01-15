91ff9bf23ca640a9d0bce36cd7f69503c819fe47
pacificLiving <kyle.corbelli@gmail.com>
2016-01-14 16:18:26 -0800

    Pass tests on graph up to && including create edges between nodes

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,31 +3,39 @@
 // ------------------------
 // Instantiate a new graph
 var Graph = function() {
+  this.nodes = {};
+  this.edges = {};
 };
 
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.nodes[node] = [];
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return node in this.nodes;
 };
 
 // ------------------------
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  delete this.nodes[node];
 };
 
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return this.nodes[fromNode].indexOf(this.nodes[toNode]) > -1;
 };
 
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.nodes[fromNode].push(this.nodes[toNode]);
+  this.nodes[toNode].push(this.nodes[fromNode]);
 };
 
 // ------------------------
