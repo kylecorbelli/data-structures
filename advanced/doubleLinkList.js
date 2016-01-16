48154c0a6294ec5f148072a820bff663e394f411
pacificLiving <kyle.corbelli@gmail.com>
2016-01-16 15:17:55 -0800

    Create DoubleLinkedList data structure

--- advanced/doubleLinkList.js
+++ advanced/doubleLinkList.js
@@ -14,35 +14,71 @@ function DoubleLinkedList() {
 DoubleLinkedList.methods = {
   addToTail: function(value) {
     if (this.tail === null) {
-      this.tail = Node(value, null, null);
+      this.tail = DoubleNode(value, null, null);
     } else {
-      this.tail.next = Node(value, this.tail, null);
+      this.tail.next = DoubleNode(value, this.tail, null);
       this.tail = this.tail.next;
     }
     if (this.head === null) this.head = this.tail;
   },
   addToHead: function(value) {
     if (this.head === null) {
-      this.head = Node(value, null, null);
+      this.head = DoubleNode(value, null, null);
     } else {
-      this.head.prev = Node(value, null, this.head);
+      this.head.prev = DoubleNode(value, null, this.head);
       this.head = this.head.prev;
     }
     if (this.tail === null) this.tail = this.head;
   },
-  removeTail: function() {},
-  removeHead: function() {},
-  containsFromTail: function(value) {},
-  containsFromHead: function(value) {}
+  removeTail: function() {
+    if (this.tail) {
+      var oldTailValue = this.tail.value;
+      if (this.tail.prev) {
+        this.tail = this.tail.prev;
+        this.tail.next = null;
+      } else {
+        this.tail = null;
+        this.head = null;
+      }
+      return oldTailValue;
+    }
+  },
+  removeHead: function() {
+    if (this.head) {
+      var oldHeadValue = this.head.value;
+      if (this.head.next) {
+        this.head = this.head.next;
+        this.head.prev = null;
+      } else {
+        this.head = null;
+        this.tail = null;
+      }
+      return oldHeadValue;
+    }
+  },
+  containsFromTail: function(value) {
+    return (function recurse(passedNode) {
+      if (passedNode.value === value) return true;
+      if (passedNode.prev === null) return false;
+      return recurse(passedNode.prev);
+    })(this.tail);
+  },
+  containsFromHead: function(value) {
+    return (function recurse(passedNode) {
+      if (passedNode.value === value) return true;
+      if (passedNode.next === null) return false;
+      return recurse(passedNode.next);
+    })(this.head);
+  }
 };
 
 
-function Node(value, prev, next) {
+function DoubleNode(value, prev, next) {
   return {
     value: value,
     next: next,
     prev: prev
-  }
+  };
 }
 
 
